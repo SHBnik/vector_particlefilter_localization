7d1f6d8a5aa98b9f911351713a976e46286084b4
nastarandarjani <68055679+nastarandarjani@users.noreply.github.com>
2020-07-15 23:28:31 +0430

    intersection updated

--- src/map.py
+++ src/map.py
@@ -58,48 +58,40 @@ def init_map(address):
     return rects
 
 
-# lims = (-1, 1)
-
-
-# plt.ylim(lims)
-# plt.xlim(lims)
-
-
-
-
-# plt.show()
-
-
-
-def _line(p1, p2):
-    A = (p1[1] - p2[1])
-    B = (p2[0] - p1[0])
-    C = (p1[0]*p2[1] - p2[0]*p1[1])
-    return A, B, -C
-
 def intersection(p1,p2,p3,p4):
-
-    L1 = _line(p1, p2)
-    L2 = _line(p3, p4)
-
-    D  = L1[0] * L2[1] - L1[1] * L2[0]
-    Dx = L1[2] * L2[1] - L1[1] * L2[2]
-    Dy = L1[0] * L2[2] - L1[2] * L2[0]
-    if D != 0:
-        x = Dx / D
-        y = Dy / D
-
-        epsilon = 0.00001
-
-        if x >= min(p1[0],p2[0])-epsilon and x <= max(p1[0],p2[0])+epsilon \
-            and x >= min(p3[0],p4[0])-epsilon and x <= max(p3[0],p4[0])+epsilon:
-            if y >= min(p1[1],p2[1])-epsilon and y <= max(p1[1],p2[1])+epsilon \
-                and y >= min(p3[1],p4[1])-epsilon and y <= max(p3[1],p4[1])+epsilon:
-                return x,y
-
-        return False
+    # example : x1 = 2 and x2 = 4
+    if (p2[0] - p1[0] == 0) and (p4[0] - p3[0] == 0):
+        # example : x1 = 2 and x2 = 2
+        if p1[0] == p3[0]:
+            return p3
+        # example : x1 = 2 and x2 = 3
+        # parallel lines
+        else:
+            return False
+    elif (p2[0] - p1[0] == 0):
+        x = p1[0]
+        m2 = (p4[1] - p3[1])/float(p4[0] - p3[0])
+        y = m2*(x - p3[0]) + p3[1]
+    elif (p4[0] - p3[0] == 0):
+        x = p3[0]
+        m1 = (p2[1] - p1[1])/float(p2[0] - p1[0])
+        y = m1*(x - p1[0]) + p1[1]
     else:
-        return False
+        m1 = (p2[1] - p1[1])/float(p2[0] - p1[0])
+        m2 = (p4[1] - p3[1])/float(p4[0] - p3[0])
+
+        # parallel lines
+        if m1 == m2 :
+            return False
+        x = (p3[1] - p1[1] + m1*p1[0] - m2*p3[0]) / (m1 - m2)
+        y = m1*(x - p1[0]) + p1[1]
+    
+    epsilon = 0.00001
+    if x >= min(p1[0],p2[0])-epsilon and x <= max(p1[0],p2[0])+epsilon and
+       y >= min(p1[1],p2[1])-epsilon and y <= max(p1[1],p2[1])+epsilon:
+        return x, y
+
+    return False
 
 def convert_point_to_line(rects):
     lines = []
